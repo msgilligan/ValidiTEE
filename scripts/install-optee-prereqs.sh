4e1f51f1633123c64e88a9f585524386aa1e13e3
Sean Gilligan <sean@msgilligan.com>
2024-05-04 12:36:23 -0500

    Add scripts/install-optee-preqeqs.sh
    This is not yet tested-on/adapted-for Debian 12 but is a simple
    copy of the Ubuntu 22.04 apt install command from:
    https://optee.readthedocs.io/en/latest/building/prerequisites.html

---
+++ scripts/install-optee-prereqs.sh
@@ -0,0 +1,54 @@
+#/bin/sh
+# Install OP-TEE prerequisites on Debian 12 with APT
+sudo apt install -y \
+    adb \
+    acpica-tools \
+    autoconf \
+    automake \
+    bc \
+    bison \
+    build-essential \
+    ccache \
+    cpio \
+    cscope \
+    curl \
+    device-tree-compiler \
+    e2tools \
+    expect \
+    fastboot \
+    flex \
+    ftp-upload \
+    gdisk \
+    git \
+    libattr1-dev \
+    libcap-ng-dev \
+    libfdt-dev \
+    libftdi-dev \
+    libglib2.0-dev \
+    libgmp3-dev \
+    libhidapi-dev \
+    libmpc-dev \
+    libncurses5-dev \
+    libpixman-1-dev \
+    libslirp-dev \
+    libssl-dev \
+    libtool \
+    libusb-1.0-0-dev \
+    make \
+    mtools \
+    netcat \
+    ninja-build \
+    python3-cryptography \
+    python3-pip \
+    python3-pyelftools \
+    python3-serial \
+    python-is-python3 \
+    rsync \
+    swig \
+    unzip \
+    uuid-dev \
+    wget \
+    xdg-utils \
+    xterm \
+    xz-utils \
+    zlib1g-dev
